5ab1dc61561da8382b10d72406bc0b3f7614446b
Sam Gammon <sam@keen.io>
2014-02-23 19:49:02 -0800

    Updating rewrite.sh to not break (hopefully) :(

--- scripts/rewrite.sh
+++ scripts/rewrite.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-gawk '/^(openssl|pcre|zlib)/ {$0=$1;gsub("^(.*:).*$","& objs/Makefile")} /^(openssl|pcre|zlib).+:/,/^$/ { if ($0 ~ "^[[:space:]]") $0="";}; {print}' <objs/Makefile.old >objs/Makefile;
+gawk '/^(openssl|pcre|zlib)/ {$0=$1;gsub("^(.*:).*$","& objs/Makefile")} /^(openssl|pcre|zlib).+:/,/^$/ { if ($0 ~ "^[[:space:]]") $0="";}; {print}' <objs/Makefile >objs/Makefile.new;
+mv -f objs/Makefile objs/Makefile.old;
+mv -f objs/Makefile.new objs/Makefile;
+sed -i objs/Makefile -re "/^\t.*$s-lcrypt$s.*\$/ {s##\t${link_order}#}";
+echo "!!! Using link order: $link_order";
+sleep 5
